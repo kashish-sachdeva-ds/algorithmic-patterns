7e0c749b6969d84c7ca7d3f80d1e9a4a0eb6d370
kashish sachdeva <kashish.sachdeva.ds@gmail.com>
2026-08-04 10:58:32 +0530

    feat: pow(x, n) done

---
+++ 06-recursion/01_pow(x,n).java
@@ -0,0 +1,29 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+
+    private double power(double x, long n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        double halfPow = power(x, n / 2);
+        double halfPowSquare = halfPow * halfPow;
+
+        if (n % 2 != 0) {
+            return x * halfPowSquare;
+        }
+
+        return halfPowSquare;
+    }
+}
