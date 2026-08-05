56ff3ebde27111b43effff9444858e86beb3626d
kashish sachdeva <kashish.sachdeva.ds@gmail.com>
2026-08-05 11:24:59 +0530

    feat: Added binary String problem using recursion

---
+++ 06-recursion/06_binaryString.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    static void binString(int n, String ans) {
+
+        if (n == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        if (ans.isEmpty() || ans.charAt(ans.length() - 1) != '1') {
+            binString(n - 1, ans + '0');
+            binString(n - 1, ans + '1');
+        } else {
+            binString(n - 1, ans + '0');
+        }
+    }
+
+    public static void main(String[] args) {
+        String ans = "";
+        binString(3, ans);
+    }
+}
