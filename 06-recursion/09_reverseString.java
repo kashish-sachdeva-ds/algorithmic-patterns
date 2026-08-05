a554fb0f52c25a6c149fdb06bfc2e944b6d3eadf
kashish sachdeva <kashish.sachdeva.ds@gmail.com>
2026-08-05 15:24:09 +0530

    feat: completed Reverse String problem using recursion

---
+++ 06-recursion/09_reverseString.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    static void printRev(String str, int idx) {
+        if (idx < 0) return;
+
+        System.out.print(str.charAt(idx));
+
+        printRev(str, idx - 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abcd";
+
+        printRev(str, str.length() - 1);
+    }
+}
