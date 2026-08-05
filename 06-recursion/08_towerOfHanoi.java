e3684b3e3cf051aa091718edc1d87ff25f0b29cd
kashish sachdeva <kashish.sachdeva.ds@gmail.com>
2026-08-05 14:40:19 +0530

    feat: completed tower of hanoi using recursion

---
+++ 06-recursion/08_towerOfHanoi.java
@@ -0,0 +1,22 @@
+public class TowerOfHanoi {
+
+    public static void towerOfHanoi(int n, String source, String destination, String helper) {
+        if (n == 1) {
+            System.out.println("Transfer disk " + n + " from " + source + " to " + destination);
+            return;
+        }
+
+        towerOfHanoi(n - 1, source, helper, destination); 
+
+        System.out.println("Transfer disk " + n + " from " + source + " to " + destination);
+
+        towerOfHanoi(n - 1, helper, destination, source);
+    }
+
+    public static void main(String[] args) {
+        int n = 3; // Number of disks
+        
+        // Function call: n disks, Source = "S", Destination = "D", Helper = "H"
+        towerOfHanoi(n, "S", "D", "H");
+    }
+}
